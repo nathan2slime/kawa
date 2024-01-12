6c79420bd4686facb7da290f6bbb357f3a7e8e6d
Jhonathan M <nathan3boss@gmail.com>
2024-01-12 13:13:29 -0300

    docs: create documentation about the project (#20)
    * fix: alt in tag images
    
    * docs: create documentation about the project

--- src/pages/index/index.page.vue
+++ src/pages/index/index.page.vue
@@ -177,7 +177,7 @@ const onToggleIsOpenDialogAddProductToClient = (e: boolean) => {
 
   .not-found {
     font-size: 0.8rem;
-    color: $color-black-alpha-100;
+    color: $color-black-200;
     margin: auto;
     padding: 20px;
   }
