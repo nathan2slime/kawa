8d243866de129f584afe02784e7437933ae3d2b3
Jhonathan M <nathan3boss@gmail.com>
2024-01-11 23:25:54 -0300

    feat: showing the navigation bar instead of the side menu on mobile screens (#15)

--- src/components/layout/header/header.component.vue
+++ src/components/layout/header/header.component.vue
@@ -15,5 +15,14 @@
   background: $color-white-100;
   border-bottom: 1px solid $color-border-200;
   backdrop-filter: blur(2px);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  position: sticky;
+  padding: 0px 20px;
+  top: 0px;
+  z-index: 20;
 }
 </style>
---
+++ src/components/layout/navbar/navbar.component.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { MenuIcon } from 'lucide-vue-next';
+</script>
+
+<template>
+  <nav class="app-navbar">
+    <img src="@/assets/favicon.svg" class="favicon" />
+
+    <MenuIcon class="toggle-menu" :width="22" :strokeWidth="1" />
+  </nav>
+</template>
+
+<style scoped lang="scss">
+.app-navbar {
+  width: 100%;
+  position: fixed;
+  top: 0px;
+  right: 0px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background: $color-primary-alpha-100;
+
+  height: 60px;
+  z-index: 30;
+  padding: 8px 20px;
+
+  .favicon {
+    width: 36px;
+  }
+
+  .toggle-menu {
+    cursor: pointer;
+  }
+
+  @include  query(md) {
+    display: none;
+  }
+}
+</style>
